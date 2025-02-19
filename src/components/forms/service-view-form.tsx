723b796d2d80a25d090e0f238d11344f5af1e29a
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-02-19 13:18:41 +0600

    service view form keep data stated successfully

--- src/components/forms/service-view-form.tsx
+++ src/components/forms/service-view-form.tsx
@@ -1,82 +1,93 @@
-import React from "react";
-import { Button, Form, Input, message, Select, Space } from "antd";
+import { Button, Form, Input, notification, Select } from "antd";
 import { Option } from "antd/es/mentions";
 
 export const ServiceViewForm = ({
-  serviceName="",
-  status="",
-  image="",
+  serviceName,
+  status,
 }: {
   serviceName: string;
   status: string;
   image?: string;
 }) => {
   const [form] = Form.useForm();
+  const [api, contextHolder] = notification.useNotification();
 
-  const onFinish = () => {
-    message.success("Submit success!");
+  const openNotification = (data: Record<string, unknown>) => {
+    const b = {
+      ...data,
+    };
+    api.open({
+      message: "Service Updated succesfully",
+      description: (
+        <pre>
+          <code>{JSON.stringify(b)}.</code>
+        </pre>
+      ),
+      duration: 2,
+    });
+  };
+  const onFinish = (values: Record<string, unknown>) => {
+    openNotification(values);
+    console.log("Received values of form: ", values);
   };
 
   const onFinishFailed = () => {
-    message.error("Submit failed!");
-  };
-
-  const onFill = () => {
-    form.setFieldsValue({
-      url: "https://taobao.com/",
-    });
+    console.log("onfinnished...");
   };
 
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        name="serviceName"
-        label="Service Name"
-        rules={[{ type: "string", min: 6 }]}
+    <>
+      {contextHolder}
+      <Form
+        form={form}
+        layout="vertical"
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Input placeholder="input placeholder" defaultValue={serviceName} />
-      </Form.Item>
-      <Form.Item
-        name="status"
-        label="Status"
-        rules={[{ message: "Please select status!" }]}
-      >
-        <Select placeholder="select status" defaultValue={status}>
-          <Option value="active">Active</Option>
-          <Option value="inactive">In Active</Option>
-        </Select>
-      </Form.Item>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: 8,
-          //   alignItems: "center",
-        }}
-      >
-        <Form.Item style={{ width: "100%" }}>
-          <Button
-            htmlType="submit"
-            style={{
-              width: "100%",
-              background:
-                "linear-gradient(to right, #9D0DFE , #AA7AD6,  #E6E6FA)",
-                color:"#FDFDFD"
-            }}
-          >
-            Update
-          </Button>
+        <Form.Item
+          name="serviceName"
+          label="Service Name"
+          rules={[{ type: "string", min: 6 }]}
+        >
+          <Input placeholder="input placeholder" defaultValue={serviceName} />
         </Form.Item>
-        <Form.Item style={{ width: "100%" }}>
-          <Button style={{ width: "100%" }}>Delete</Button>
+        <Form.Item
+          name="status"
+          label="Status"
+          rules={[{ message: "Please select status!" }]}
+        >
+          <Select placeholder="select status" defaultValue={status}>
+            <Option value="active">Active</Option>
+            <Option value="inactive">In Active</Option>
+          </Select>
         </Form.Item>
-      </div>
-    </Form>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: 8,
+            //   alignItems: "center",
+          }}
+        >
+          <Form.Item style={{ width: "100%" }}>
+            <Button
+              htmlType="submit"
+              style={{
+                width: "100%",
+                background:
+                  "linear-gradient(to right, #9D0DFE , #AA7AD6,  #E6E6FA)",
+                color: "#FDFDFD",
+              }}
+            >
+              Update
+            </Button>
+          </Form.Item>
+          <Form.Item style={{ width: "100%" }}>
+            <Button style={{ width: "100%" }}>Delete</Button>
+          </Form.Item>
+        </div>
+      </Form>
+    </>
   );
 };
